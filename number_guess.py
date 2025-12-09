9c07bb66e7cc39d2630517b019a4f3eaeb18ffc9
dogukan_o <dogukan.ozgen@gmail.com>
2025-12-09 12:00:40 -0800

    First project: Number guessing

---
+++ number_guess.py
@@ -0,0 +1,27 @@
+# 1. Computer picks a random number (1-100)
+# 2. User guesses
+# 3. Computer says "too high" or "too low"
+# 4. Repeat until user guesses correctly
+# 5. Show how many tries it took
+# 6. Ask if they want to play again
+
+import random
+
+while True:
+    number_to_guess = random.randint(1, 100)
+    tries = 0   
+
+    while True:
+        user_number = int(input ("Guess a number between 1 and 100: "))
+        tries += 1
+        if user_number < number_to_guess: 
+                print(f"Too low!")
+        elif user_number > number_to_guess:
+                print(f"Too high!")
+        else:
+            print("Correct!")
+            break    
+    play_again = (input ('Play again? (y/n): ').lower())
+    if play_again != 'y':
+        print ("Thanks for playing!")
+        break
