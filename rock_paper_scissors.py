ad99ec3e898a354de9b095b3b203945a2c7d65b8
dogukan_o <dogukan.ozgen@gmail.com>
2025-12-18 12:55:35 -0800

    Add a scorebard to Rock Paper Scissors

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -5,6 +5,11 @@ print ("Welcome to Rock, Paper, Scissors!")
 # Define possible choices
 choices = ["rock", "paper", "scissors"]
 
+#scores 
+
+user_score = 0
+computer_score = 0
+
 while True:
     player = input ("Enter your choice (rock, paper, scissors): ").lower()
     if player not in choices:
@@ -22,10 +27,13 @@ while True:
             (player == "paper" and computer == "rock") or \
             (player == "scissors" and computer == "paper"): 
             print("You win!")
+            user_score += 1
     else:
             print("Computer wins!")
-    
+            computer_score += 1
+    print(f"Score - You: {user_score} vs. Computer: {computer_score}")        
     play_again = input ("Play again? (y/n):").lower()
     if play_again != 'y':
         print("Thanks for playing!")
+        print (f"Final Score - You: {user_score} vs. Computer: {computer_score}")
         break
