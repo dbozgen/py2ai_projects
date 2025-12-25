40e70310afc724abe2cdc7fef5a14bcf4f8ed24b
dogukan_o <dogukan.ozgen@gmail.com>
2025-12-24 20:56:08 -0800

    Added the number of guesses feature.

--- number_guess.py
+++ number_guess.py
@@ -8,20 +8,19 @@
 import random
 
 while True:
-    number_to_guess = random.randint(1, 100)
-    tries = 0   
-
-    while True:
-        user_number = int(input ("Guess a number between 1 and 100: "))
-        tries += 1
-        if user_number < number_to_guess: 
-                print(f"Too low!")
-        elif user_number > number_to_guess:
-                print(f"Too high!")
-        else:
-            print("Correct!")
-            break    
-    play_again = (input ('Play again? (y/n): ').lower())
-    if play_again != 'y':
-        print ("Thanks for playing!")
-        break
+        number_to_guess = random.randint(1, 100)
+        tries = 0   
+        while True:
+                user_number = int(input ("Guess a number between 1 and 100: "))
+                tries += 1
+                if user_number < number_to_guess: 
+                        print(f"Too low!")
+                elif user_number > number_to_guess:
+                        print(f"Too high!")
+                else:
+                        print(f"Amazing! You guessed it in {tries} tries.")
+                        break    
+        play_again = (input ('Play again? (y/n): ').lower())
+        if play_again != 'y':
+                print ("Thanks for playing!")
+                break
