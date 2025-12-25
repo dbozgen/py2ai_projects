d6649ccbb1ba9b7099eba66d9305542f19b349df
dogukan_o <dogukan.ozgen@gmail.com>
2025-12-24 21:15:13 -0800

    added dictionary logic instead of long elif statement

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -3,6 +3,12 @@ import random
 print ("Welcome to Rock, Paper, Scissors!")
 
 # Define possible choices
+wins = {
+    "rock": "scissors",      # rock beats scissors
+    "paper": "rock",         # paper beats rock
+    "scissors": "paper"      # scissors beats paper
+}
+
 choices = ["rock", "paper", "scissors"]
 
 #scores 
@@ -23,17 +29,24 @@ while True:
 
     if player == computer:
             print("It's a tie!")
-    elif (player == "rock" and computer == "scissors") or \
-            (player == "paper" and computer == "rock") or \
-            (player == "scissors" and computer == "paper"): 
+    elif wins[player] == computer:
             print("You win!")
             user_score += 1
     else:
             print("Computer wins!")
             computer_score += 1
-    print(f"Score - You: {user_score} vs. Computer: {computer_score}")        
+    print(f"Score - You: {user_score} vs. Computer: {computer_score}") 
+    
+    # Check if someone won best of 5
+    if user_score == 3:
+            print ("You won the best of 5 series!")
+            print (f"Final Score - You: {user_score} vs. Computer: {computer_score}")
+            break 
+    elif computer_score == 3:
+            print ("Computer won the best of 5 series!")
+            print (f"Final Score - You: {user_score} vs. Computer: {computer_score}")
+            break
     play_again = input ("Play again? (y/n):").lower()
     if play_again != 'y':
         print("Thanks for playing!")
-        print (f"Final Score - You: {user_score} vs. Computer: {computer_score}")
         break
